e98170d322f53fa6b056017f908b1e841683607b
ksudharshan1706 <kamavaramsudharshan1706@gmail.com>
2022-12-05 10:24:12 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,42 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useState } from "react";
+import "./App.css";
 // import Sudharshan from ''
 function App() {
+  const users = [
+    {
+      name: "sudharshan",
+      image: "https://images.pexels.com/photos/259915/pexels-photo-259915.jpeg",
+    },
+    {
+      name: "uma",
+      image:
+        "https://static.vecteezy.com/packs/media/vectors/term-bg-1-666de2d9.jpg",
+    },
+    {
+      name: "Sreenu",
+      image:
+        "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
+    },
+  ];
   return (
-    <div className='App'>
-      <Welcome name = "sudharshan" image = "https://images.pexels.com/photos/259915/pexels-photo-259915.jpeg"/>
-      <Welcome name = "uma" image ="https://static.vecteezy.com/packs/media/vectors/term-bg-1-666de2d9.jpg"/>
-      <Welcome name="Sreenu" image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"/>
-      </div>
-  )
-function Welcome({image,name}){
-  // let name = "sudharshan";
-  // const{image,name} = props
+    <div className="App">
+      {users.map((user) => (
+        <Welcome image={user.image} name={user.name} />
+      ))}
+    </div>
+  );
+}
+function Welcome({ image, name }) {
+  // console.log("here", { name });
   return (
     <div>
-      <img className='img-dimensions' src={image} alt={name}></img>
+      <img className="img-dimensions" src={image} alt={name}></img>
       <h1>Hi {name}, 👌😍😁</h1>
-      </div>
-  )
+      <Counter />
+      {/* <DisCounter /> */}
+    </div>
+  );
 }
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
+function Counter() {
+  let [like, setLike] = useState(0);
+  let [dislike, setDisLike] = useState(0);
+
+  const messageStyle = {
+    display: like - dislike >= 10 ? "block" : "none",
+  };
+  return (
+    <div>
+      <h2 style={messageStyle}>You are Awesome</h2>
+      {like - dislike >= 10 ? <h2>You Are Awesome 😍😍</h2> : null}
+      <button onClick={() => setLike(like + 1)}>👍{like}</button>
+      <button onClick={() => setDisLike(dislike + 1)}>👎{dislike}</button>
+
+      {/* <h2>{like}</h2> */}
+    </div>
+  );
 }
 
 export default App;
